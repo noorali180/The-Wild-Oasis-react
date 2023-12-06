d055e01a18d9d8e20958635e5977b2c393793fd6
Noor Ali <nooralib180@gmail.com>
2023-12-06 10:19:04 +0500

    updating settings using custom hook

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -3,6 +3,7 @@ import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 import Spinner from "../../ui/Spinner";
+import { useUpdateSetting } from "./hooks/useUpdateSetting";
 import { useSettings } from "./hooks/usesettings";
 
 function UpdateSettingsForm() {
@@ -16,24 +17,45 @@ function UpdateSettingsForm() {
       breakfastPrice,
     } = {},
   } = useSettings();
+  const { isUpdating, updateSetting } = useUpdateSetting();
+
+  function handleEvent(e, field) {
+    const value = e.target.value;
+
+    updateSetting({ [field]: value });
+  }
 
   if (isLoading) return <Spinner />;
 
-  if(error) return <PageNotFound />
+  if (error) return <PageNotFound />;
 
   return (
     <Form>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" id="min-nights" defaultValue={minBookingLength} />
+        <Input
+          type="number"
+          id="min-nights"
+          defaultValue={minBookingLength}
+          disabled={isUpdating}
+          onBlur={(e) => handleEvent(e, "minBookingLength")}
+        />
       </FormRow>
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
+        <Input
+          type="number"
+          id="max-nights"
+          defaultValue={maxBookingLength}
+          disabled={isUpdating}
+          onBlur={(e) => handleEvent(e, "maxBookingLength")}
+        />
       </FormRow>
       <FormRow label="Maximum guests/booking">
         <Input
           type="number"
           id="max-guests"
           defaultValue={maxGuestsPerBooking}
+          disabled={isUpdating}
+          onBlur={(e) => handleEvent(e, "minGuestsPerBooking")}
         />
       </FormRow>
       <FormRow label="Breakfast price">
@@ -41,6 +63,8 @@ function UpdateSettingsForm() {
           type="number"
           id="breakfast-price"
           defaultValue={breakfastPrice}
+          disabled={isUpdating}
+          onBlur={(e) => handleEvent(e, "breakfastPrice")}
         />
       </FormRow>
     </Form>
--- src/features/settings/hooks/useSettings.js
+++ src/features/settings/hooks/useSettings.js
@@ -1,11 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { getSettings } from "../../../services/apiSettings";
 
-export function useSettings(){
-    const {isLoading, error, data: settings} = useQuery({
-        queryKey: ['settings'],
-        queryFn: getSettings,
-    })
-    
-    return {isLoading, error, settings}
+export function useSettings() {
+  const {
+    isLoading,
+    error,
+    data: settings,
+  } = useQuery({
+    queryKey: ["settings"],
+    queryFn: getSettings,
+  });
+
+  return { isLoading, error, settings };
 }
