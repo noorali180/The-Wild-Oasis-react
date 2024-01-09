515f2fbd9b4f7a3c5cd5281be1d385867cfe9c3d
Noor Ali <nooralib180@gmail.com>
2024-01-09 20:34:07 +0500

    creating a reusable Select Component

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -21,7 +21,6 @@ function CabinTableOperations() {
           { value: "regularPrice-desc", label: "Sort by price (high first)" },
           { value: "maxCapacity-asc", label: "Sort by capacity (low first)" },
           { value: "maxCapacity-desc", label: "Sort by capacity (high first)" },
-          { value: "name-desc", label: "Sort by name (Z-A)" },
         ]}
       />
     </TableOperation>
